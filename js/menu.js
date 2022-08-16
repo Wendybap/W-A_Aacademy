0661f94af274b50e8dea8c054e1f36e8a53bf62b
wendy Arcia <wendy_342@hotmail.com>
2022-08-15 23:00:04 -0300

    Home responsive

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById("btn--menu");
+let nav = document.getElementById("navbar");
+
+btnMenu.addEventListener("click", function () {
+  nav.classList.toggle("showNav");
+});
